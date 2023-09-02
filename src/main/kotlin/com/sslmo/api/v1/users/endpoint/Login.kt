f6d255967e3d01b45be61dc83c7f9aab456a48fd
Delly <daehyeon1999@naver.com>
2023-09-02 22:35:42 +0900

    [UPDATE] API 수정
    - 소셜 로그인 엔드포인트를 통합

--- src/main/kotlin/com/sslmo/api/v1/users/endpoint/Login.kt
+++ src/main/kotlin/com/sslmo/api/v1/users/endpoint/Login.kt
@@ -5,7 +5,6 @@ import com.sslmo.api.v1.users.models.EmailLoginRequest
 import com.sslmo.api.v1.users.models.SocialLoginRequest
 import com.sslmo.api.v1.users.models.User
 import com.sslmo.api.v1.users.service.UserService
-import com.sslmo.models.SignType
 import com.sslmo.utils.setCookie
 import io.github.smiley4.ktorswaggerui.dsl.post
 import io.ktor.http.*
@@ -50,107 +49,31 @@ fun Route.login() {
 			}
 		}
 
-		// 카카오 로그인
-		post("/kakao", {
+		// 소셜 로그인
+		post("/social", {
 			tags = listOf("User")
-			summary = "카카오 로그인"
+			summary = "소셜 로그인"
 			request {
 				body<SocialLoginRequest>()
 			}
 			response {
 				HttpStatusCode.OK to {
-					description = "success"
+					description = "로그인 성공"
 					body<Response.Success<User>>()
 				}
 				HttpStatusCode.BadRequest to {
-					description = "bad_request"
+					description = "로그인 실패"
 					body<Response.Error<*>>()
 				}
 			}
 		}) {
 			val request = call.receive<SocialLoginRequest>()
-			val user = userService.socialLogin(request.socialId, SignType.KAKAO)
-			val token = user.generateToken(application.environment.config)
-			this.setCookie(token.accessToken, token.refreshToken)
-			call.respond(HttpStatusCode.OK, Response.Success(user, "로그인에 성공하였습니다."))
-
-		}
-
-		// 네이버 로그인
-		post("/naver", {
-			tags = listOf("User")
-			summary = "네이버 로그인"
-			request {
-				body<SocialLoginRequest>()
-			}
-			response {
-				HttpStatusCode.OK to {
-					description = "success"
-					body<Response.Success<User>>()
-				}
-				HttpStatusCode.BadRequest to {
-					description = "bad_request"
-					body<Response.Error<*>>()
-				}
-			}
-		}) {
-			val request = call.receive<SocialLoginRequest>()
-			val user = userService.socialLogin(request.socialId, SignType.NAVER)
+			val user = userService.socialLogin(request.socialId, request.type)
 			val token = user.generateToken(application.environment.config)
 			this.setCookie(token.accessToken, token.refreshToken)
 			call.respond(HttpStatusCode.OK, Response.Success(user, "로그인에 성공하였습니다."))
 		}
 
-		// 구글 로그인
-		post("/google", {
-			tags = listOf("User")
-			summary = "Google 로그인"
-			request {
-				body<SocialLoginRequest>()
-			}
-			response {
-				HttpStatusCode.OK to {
-					description = "success"
-					body<Response.Success<User>>()
-				}
-				HttpStatusCode.BadRequest to {
-					description = "bad_request"
-					body<Response.Error<*>>()
-				}
-			}
-		}) {
-			val request = call.receive<SocialLoginRequest>()
-			val user = userService.socialLogin(request.socialId, SignType.GOOGLE)
-			val token = user.generateToken(application.environment.config)
-			this.setCookie(token.accessToken, token.refreshToken)
-			call.respond(HttpStatusCode.OK, Response.Success(user, "로그인에 성공하였습니다."))
-
-		}
-
-		// apple login
-		post("/apple", {
-			tags = listOf("User")
-			summary = "Apple로 로그인"
-			request {
-				body<SocialLoginRequest>()
-			}
-			response {
-				HttpStatusCode.OK to {
-					description = "success"
-					body<Response.Success<User>>()
-				}
-				HttpStatusCode.BadRequest to {
-					description = "bad_request"
-					body<Response.Error<*>>()
-				}
-			}
-		}) {
-			val request = call.receive<SocialLoginRequest>()
-			val user = userService.socialLogin(request.socialId, SignType.KAKAO)
-			val token = user.generateToken(application.environment.config)
-			this.setCookie(token.accessToken, token.refreshToken)
-			call.respond(HttpStatusCode.OK, Response.Success(user, "로그인에 성공하였습니다."))
-		}
 	}
 
 }
--- src/main/kotlin/com/sslmo/api/v1/users/models/LoginRequest.kt
+++ src/main/kotlin/com/sslmo/api/v1/users/models/LoginRequest.kt
@@ -1,5 +1,6 @@
 package com.sslmo.api.v1.users.models
 
+import com.sslmo.models.SignType
 import io.swagger.v3.oas.annotations.media.Schema
 import kotlinx.serialization.Serializable
 
@@ -12,5 +13,10 @@ data class EmailLoginRequest(
 
 @Serializable
 data class SocialLoginRequest(
-	@field:Schema(required = true, name = "social_id") val socialId: String
-)
+	@field:Schema(required = true, name = "social_id") val socialId: String,
+	@field:Schema(required = true) val type: SignType
+) {
+	init {
+		assert(type != SignType.EMAIL) // SignType이 EMAIL인 경우 assert 에러를 발생
+	}
+}
