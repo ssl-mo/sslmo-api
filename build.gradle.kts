7d950a627ffffa71cc9b68273db0033f86749157
Delly <daehyeon1999@naver.com>
2023-10-14 14:34:12 +0900

    [UPDATE] UserTable
    - UserTable을 Exposed 형식으로 변경
    - UserEntity 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
 	implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
 	implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
 	implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
-	implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+	implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
 
 	implementation("com.zaxxer:HikariCP:5.0.1")
 
